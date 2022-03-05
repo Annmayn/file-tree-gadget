fc6880ebacd898b8cb298f7e4c3704f27cbdb455
Annmayn <ntvirus333@gmail.com>
2022-03-05 08:50:58 +0545

    convert folder to functional; fix child element resetting

--- src/App.js
+++ src/App.js
@@ -1,83 +1,64 @@
 import React, { useState } from 'react';
 import './App.css'
 
-// TODO: add functionality to add/remove files/folders
 function App() {
   return <div>
-    {/* <Folder name="Desktop">
-      <Folder name="C">
-        <File name="dog.jpg" />
-      </Folder>
-      <Folder name="D">
-        <File name="eel.png" />
-        <File name="fish.png" />
-        <File name="horse_sound.mp3" />
-        <File name="cat.mp4" />
-      </Folder>
-    </Folder>
-    <Folder name="Applications" />
-    <File name="cat.jpg" /> */}
-    <Folder name="/" />
+    <Folder key="null" name="/" />
   </div>
 }
 
-class Folder extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      isOpen: true,
-      add_new: '',
-      num: 1
-    }
+const Folder = (props) => {
+  const [state, setState] = useState({
+    data: [],
+    isOpen: true,
+    name: '',
+  })
+
+  const handleExpandClick = () => {
+    setState(prevState => ({ ...prevState, isOpen: !state.isOpen }));
+  }
+
+  const handleInput = (e) => {
+    setState(prevState => ({ ...prevState, "name": e }))
+  }
+
+  const handleClickAddFolder = () => {
+    const new_section = { type: "folder", name: state.name }
+    setState(prevState => ({ ...prevState, data: [...prevState.data, new_section], num: prevState.num++ }))
+    handleInput('')
+  }
+
+  const handleClickAddFile = () => {
+    let new_section = { type: "file", name: state.name }
+    setState(prevState => ({ ...prevState, data: [...prevState.data, new_section] }))
+    handleInput('')
   }
 
 
-  render() {
-    // let [isOpen, setIsOpen] = useState(false);
-    let { name, children } = this.props;
-    let child = this.state.data;
-
-    const handleExpandClick = () => {
-      this.setState({ isOpen: !this.state.isOpen });
+  return <div>
+    <span className='folder-info' onClick={handleExpandClick}>
+      <i className={state.isOpen ? 'blue folder open outline icon' : 'blue folder icon'}></i>
+      <i className={state.isOpen ? 'angle down icon' : 'angle right icon'}></i>
+      {props.name}
+    </span>
+    <input value={state.name} onChange={(e) => handleInput(e.target.value)} type="text" />
+    <span className='folder-add' title='Create new file' onClick={handleClickAddFile}>
+      <i className='file icon' />
+    </span>
+    <span className='folder-add' title="Create new folder" onClick={handleClickAddFolder}>
+      <i className='folder icon' />
+    </span>
+    {
+      state.isOpen &&
+      state.data.map((datum, index) => {
+        return <div key={index} className='folder-child'>
+          {datum.type == "folder" ? <Folder name={datum.name} /> : <File name={datum.name} />}
+        </div>
+      }
+      )
     }
+  </div>
 
-    const handleInput = (e) => {
-      this.setState({ 'add_new': e })
-    }
-
-    const handleAddClickFolder = () => {
-      var data = this.state.data
-      var new_section = <Folder name={this.state.add_new} />
-      data.push(new_section)
-      this.setState({ 'data': data })
-      handleInput('')
-    }
-
-    const handleAddClickFile = () => {
-      var data = this.state.data
-      var new_section = <File name={this.state.add_new} />
-      data.push(new_section)
-      this.setState({ 'data': data })
-      handleInput('')
-    }
-
-    return <div>
-      <span className='folder-info' onClick={handleExpandClick}>
-        <i className={this.state.isOpen ? 'blue folder open outline icon' : 'blue folder icon'}></i>
-        <i className={this.state.isOpen ? 'angle down icon' : 'angle right icon'}></i>
-        {name}
-      </span>
-      <input value={this.state.add_new} onChange={(e) => handleInput(e.target.value)} type="text" />
-      <span className='folder-add' title='Create new file' onClick={handleAddClickFile}>
-        <i className='file icon' />
-      </span>
-      <span className='folder-add' title="Create new folder" onClick={handleAddClickFolder}>
-        <i className='folder icon' />
-      </span>
-      {this.state.isOpen && child.map((it) => { return <div key={this.state.num++} className='folder-child'> {it} </div> })}
-    </div>
-  }
 }
 
 const File = (props) => {
